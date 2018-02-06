9aac48c81dbd5cb00e32555e7fd0d67de14a977c
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2018-02-06 07:01:31 -0500

    Use 1 line sol for local maxima, hw3

--- hw3/golf.hs
+++ hw3/golf.hs
@@ -7,13 +7,11 @@ skipsN n ts = (drop (n - 1) (take n ts)) ++ (skipsN n (drop n ts))
 skips :: [t] -> [[t]]
 skips t = [skipsN k t | k <- [1..(length t)]]
 
-extractLM :: [Integer] -> [Integer]
-extractLM (x:(y:(z:[])))
-	| (y > x) && (y > z) = [y]
-extractLM _ = []
-
 localMaxima :: [Integer] -> [Integer]
-localMaxima ls = foldl (++) [] (map extractLM [ take 3 (drop i ls) | i <- [0..(length(ls)-3)]])
+localMaxima ls = map (\(x, y, z) -> y) (filter (\(x, y, z) -> (y > x) && (y > z)) (zip3 ls (drop 1 ls) (drop 2 ls)))
+
+histogram :: [Int] -> String
+histogram _ = ""
 
 main = do
     print (skips "ABCD")
@@ -21,4 +19,9 @@ main = do
     print (skips [1])
     print (skips [True,False])
     print (length (skips []))
-    print (localMaxima [2,9,5,6,1])
+    let ls = [2,9,5,6,1,7,3,5,6,7,8,9,6]
+    print (ls)
+    print (localMaxima ls)
+
+    let histogram_data = [1,4,5,4,6,6,3,4,2,4,9]
+    print (histogram histogram_data)
