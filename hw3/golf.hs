5bca449f21eb1bc9d0b86bb69035ac9412c6ee61
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2018-02-06 07:57:25 -0500

    Couple of helper functions for histogram, hw3

--- hw3/golf.hs
+++ hw3/golf.hs
@@ -1,17 +1,29 @@
 module Golf where
 
+-- Begin skips
 skipsN :: Int -> [t] -> [t]
 skipsN _ [] = []
 skipsN n ts = (drop (n - 1) (take n ts)) ++ (skipsN n (drop n ts))
 
 skips :: [t] -> [[t]]
 skips t = [skipsN k t | k <- [1..(length t)]]
+-- End skips
 
+-- Begin Local maxima from a list of integers
 localMaxima :: [Integer] -> [Integer]
 localMaxima ls = map (\(x, y, z) -> y) (filter (\(x, y, z) -> (y > x) && (y > z)) (zip3 ls (drop 1 ls) (drop 2 ls)))
+-- End Local maxima from a list of integers
+
+-- Begin histogram
+getCount :: Int -> [Int] -> Int
+getCount n xs = length (filter (n==) xs)
+
+getCounts :: [Int] -> [Int]
+getCounts xs = [getCount i xs | i <- [0..9]]
 
 histogram :: [Int] -> String
 histogram _ = ""
+-- End histogram
 
 main = do
     print (skips "ABCD")
@@ -24,4 +36,5 @@ main = do
     print (localMaxima ls)
 
     let histogram_data = [1,4,5,4,6,6,3,4,2,4,9]
+    print (getCounts histogram_data)
     print (histogram histogram_data)
